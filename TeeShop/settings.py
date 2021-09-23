dd725f547178f3f4de70af347a1fdd23b6736378
FIRST_NAME LAST_NAME <prasenjitpawar22@gmail.com>
2021-09-23 22:46:40 +0530

    tweaked app for herko another try

--- TeeShop/settings.py
+++ TeeShop/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+import django_heroku
+import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -51,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'TeeShop.urls'
@@ -135,6 +139,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStoroage'
+
 STATIC_ROOT = 'D:/Build/TeeShop/asset'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/product-images')
 
@@ -143,3 +149,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/product-images')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
